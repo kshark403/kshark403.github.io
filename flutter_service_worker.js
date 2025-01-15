d6666cc3f40a64278ae67c25573646082a44b28f
Chaovapan Panmee <chaovapan_pan_niit@tdem.toyota-asia.com>
2025-01-15 18:03:59 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -729,14 +729,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b7651dcf53c11735278333b4fab61a5a
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5f04be7eed72c76329c313da6f440996",
+"flutter_bootstrap.js": "a8e1df7d33981a9130acede13b601374",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "9582aad844d4d6e424b29bc6bc37efff",
 "/": "9582aad844d4d6e424b29bc6bc37efff",
-"main.dart.js": "f89c0f4b7be77ccf202e7dfe86d12fd2",
+"main.dart.js": "1747ba945b29fed106abcd561074152d",
 "manifest.json": "35f6a7e50a4a61c9c79d0fc14579ae3b",
 "version.json": "c49e4621b80a524d89920862659ef087"};
 // The application shell files that are downloaded before a service worker can
