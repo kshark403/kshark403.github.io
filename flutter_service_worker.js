5f68696b0b632e4888f4558802e6f7f7b99cfba8
Chaovapan Panmee <chaovapan_pan_niit@tdem.toyota-asia.com>
2025-02-08 09:43:35 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -737,8 +737,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "b7651dcf53c11735278333b4fab61a5a
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7a485db6df6d1b10c4fe735c95aaf8d8",
 "/": "7a485db6df6d1b10c4fe735c95aaf8d8",
-"main.dart.js": "352f49143aad3006b19c271994a45400",
+"main.dart.js": "4fd76b4f258f3f4402982c5a25b44095",
 "manifest.json": "35f6a7e50a4a61c9c79d0fc14579ae3b",
+"TermCondtions.html": "7697cccf8d8f306a1ead58afff5a988e",
 "version.json": "c49e4621b80a524d89920862659ef087"};
 // The application shell files that are downloaded before a service worker can
 // start.
