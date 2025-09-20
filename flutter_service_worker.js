de70c0a58c98ca1664c8c6fd03856a004e0b6074
Chaovapan Panmee <chaovapan_pan_niit@tdem.toyota-asia.com>
2025-09-20 14:06:48 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -738,7 +738,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c4afcec9841c9e2477344f51e4b4ed6f
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "8b5a0fb350315f3cba0ecc1d46b8c2a1",
 "/": "8b5a0fb350315f3cba0ecc1d46b8c2a1",
-"main.dart.js": "8d8106614d16dce2459416a18558dcbd",
+"main.dart.js": "6bbc1599078c88e239ade4acc479e8fd",
 "manifest.json": "0764b6c9a0d12d15d536fc38b25d296d",
 "my_js.js": "cd1452ae6582b902b214d9b18725d2a9",
 "version.json": "98ea962230842745a4c06f87aa348b53"};
